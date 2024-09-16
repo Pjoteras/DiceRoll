8490caf1ced570a595cb4835e17116b642e1775a
Pjoteras <voxli682@gmail.com>
2024-09-16 12:14:11 +0200

    Comit na 2

--- DiceRoll/MainPage.xaml.cs
+++ DiceRoll/MainPage.xaml.cs
@@ -1,20 +1,16 @@
 ﻿namespace DiceRoll {
     public partial class MainPage : ContentPage {
-        int count = 0;
+        
 
         public MainPage() {
             InitializeComponent();
         }
 
         private void OnCounterClicked(object sender, EventArgs e) {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            
+            Random r = new Random();
+            int diceRoll = r.Next(1, 7);
+            CounterBtn.Text = "Wynik rzutu;" + diceRoll.ToString();
         }
     }
 
