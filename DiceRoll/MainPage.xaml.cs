b638d2d4fb0cad7c68cc2a159c547a2b69a5d857
Pjoteras <voxli682@gmail.com>
2024-09-16 12:21:28 +0200

    Comit na 4

--- DiceRoll/MainPage.xaml.cs
+++ DiceRoll/MainPage.xaml.cs
@@ -10,7 +10,19 @@
             
             Random r = new Random();
             int diceRoll = r.Next(1, 7);
-            CounterBtn.Text = "Wynik rzutu;" + diceRoll.ToString();
+            if (K4.IsChecked) {
+                diceRoll = r.Next(1, 5);
+            }
+            if (K6.IsChecked) {
+                diceRoll = r.Next(1, 7);
+            }
+            if (K10.IsChecked) {
+                diceRoll = r.Next(1, 11);
+            }
+            if (K12.IsChecked) {
+                diceRoll = r.Next(1, 13);
+            }
+            RollLabel.Text = "Wynik rzutu: " + diceRoll.ToString();
         }
     }
 
